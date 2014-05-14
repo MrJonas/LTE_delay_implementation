7bce73d431fa83d2055d89a2be32cef036d0a560
MrJonas <jonas.l.antanaitis@gmail.com>
2014-05-14 17:09:42 +0300

    Rikiavimo funkcija atlieka velinimas.m.

--- velinimas.m
+++ velinimas.m
@@ -24,6 +24,7 @@ function velinimas( file_name, SINR_file, RSRP_file, MCL, f, BW)
             pos_x(j) = simulation_traces.UE_traces(j).position(1);
             pos_y(j) = simulation_traces.UE_traces(j).position(2);
     end
+    
     %Atliekame skaičiavimus:
 	fid = fopen(file_name);
 	tline = fgets(fid);
@@ -57,7 +58,7 @@ function velinimas( file_name, SINR_file, RSRP_file, MCL, f, BW)
 	end
 	fclose(fid);
     
-    %Sukialeme galutinius rezultatus į masiva ir surikiuojame:
+    %Sukialeme galutinius rezultatus į masiva:
     p = size(simulation_traces.UE_traces);
     for j = 1:p(2)
         SINR_array(j,1) = SINR(j); 
@@ -72,10 +73,43 @@ function velinimas( file_name, SINR_file, RSRP_file, MCL, f, BW)
         RSRP_array(j,4) = pos_x(j);
         RSRP_array(j,5) = pos_y(j);
     end
+    
+    %Irasome i masivus pries tai faile išsaugotus duomenis:
+    %SINR failui:
+    if exist(SINR_file, 'file')
+        sinrSaveFile = fopen(SINR_file);
+        line = fgets(sinrSaveFile);
+        while ischar(line)
+            j = p(2) + 1;
+            A = sscanf(line,'%f');
+            SINR_array(j, 1) = A(1);
+            SINR_array(j, 2) = A(2);
+            SINR_array(j, 3) = A(3);
+            SINR_array(j, 4) = A(4);
+            SINR_array(j, 5) = A(5);
+            j = j + 1;
+        end
+    end 
+    %RSRP failui:
+    if exist(RSRP_file, 'file')       
+        rsrpSaveFile = fopen(RSRP_file);
+        line = fgets(rsrpSaveFile);
+        while ischar(line)
+            j = p(2) + 1;
+            A = sscanf(line,'%f');
+            RSRP_array(j, 1) = A(1);
+            RSRP_array(j, 2) = A(2);
+            RSRP_array(j, 3) = A(3);
+            RSRP_array(j, 4) = A(4);
+            RSRP_array(j, 5) = A(5);
+            j = j + 1;
+        end
+    end
+    %surikiuojame masiva
     SINR_array = sortrows(SINR_array,1);
     RSRP_array = sortrows(RSRP_array,1);
     
-%Irasome rezultatus pagal SINR:
+    %Irasome rezultatus. SINR:
     %SINR_save_file = fopen('SINR_MAP.txt', 'at');
     SINR_save_file = fopen(SINR_file, 'at');
     for j = 1:p(2)
@@ -89,7 +123,7 @@ function velinimas( file_name, SINR_file, RSRP_file, MCL, f, BW)
     end               
     fclose(SINR_save_file);
     
-%Irasome rezultatus pagal RSRP:
+    %Irasome rezultatus. RSRP:
     %RSRP_save_file = fopen('RSRP_MAP.txt', 'at');
     RSRP_save_file = fopen(RSRP_file, 'at');    
     for j = 1:p(2)
