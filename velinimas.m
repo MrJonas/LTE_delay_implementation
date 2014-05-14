701636f17625dac7c67873a1eb06c0242de110bd
MrJonas <jonas.l.antanaitis@gmail.com>
2014-05-14 16:52:07 +0300

    Prideti duomenu skaiciavimo failai.

---
+++ velinimas.m
@@ -0,0 +1,105 @@
+function velinimas( file_name, SINR_file, RSRP_file, MCL, f, BW)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+    %Situ turbut nereikia:
+	sum_delay = [];
+    sum_number = [];
+    SINR = [];
+    RSRP = [];
+    pos_x = [];
+    pos_y = [];
+    %Ikeleme workspace:
+    simulation_traces = evalin('base', 'simulation_traces');
+    eNodeBs_sectors = evalin('base', 'eNodeBs_sectors');
+    %Priskiriame pradines vertes:
+    p = size(simulation_traces.UE_traces);
+    for j= 1:p(2)
+            sum_delay(j) = 0;
+            sum_number(j) = 0;
+            delay(j) = 0;
+            thput(j) = simulation_traces.UE_traces(j).average_throughput_Mbps; 
+            SINR(j) = simulation_traces.UE_traces(j).wideband_SINR(1);
+            RSRP(j) = 0;          
+            pos_x(j) = simulation_traces.UE_traces(j).position(1);
+            pos_y(j) = simulation_traces.UE_traces(j).position(2);
+    end
+    %Atliekame skaičiavimus:
+	fid = fopen(file_name);
+	tline = fgets(fid);
+	while ischar(tline)
+            %Nuskaitome eilute:
+    		A = sscanf(tline,'%f');
+            %RSRP:
+            d1 = ( simulation_traces.UE_traces(A(1)).position(1) - eNodeBs_sectors(simulation_traces.UE_traces(A(1)).attached_eNodeB(1)).parent_eNodeB.pos(1)).^2;   
+            d2 = (simulation_traces.UE_traces(A(1)).position(2) - eNodeBs_sectors(simulation_traces.UE_traces(A(1)).attached_eNodeB(1)).parent_eNodeB.pos(2)).^2;
+            d = sqrt(d1 + d2);
+            Loss = 10*log10(4*3.14/300000000*d*f);
+            %RBs:
+            if (BW == 5)
+                RBs = 25;
+            elseif (BW == 10)
+                RBs = 50;          
+            elseif (BW == 20)
+                RBs = 100;
+            end
+            %RSRP:
+            RSRP(A(1)) = 40 - Loss - MCL - 10*log10(12*RBs);
+            % Thr
+            thput(A(1)) = simulation_traces.UE_traces(A(1)).average_throughput_Mbps;
+            %SINR:
+            SINR(A(1)) = simulation_traces.UE_traces(A(1)).wideband_SINR(1);
+            %Delay:
+            sum_delay(A(1)) = sum_delay(A(1)) + A(2);
+            sum_number(A(1)) = sum_number(A(1)) + 1;
+            delay(A(1)) = sum_delay(A(1))/sum_number(A(1));
+    		tline = fgets(fid);
+	end
+	fclose(fid);
+    
+    %Sukialeme galutinius rezultatus į masiva ir surikiuojame:
+    p = size(simulation_traces.UE_traces);
+    for j = 1:p(2)
+        SINR_array(j,1) = SINR(j); 
+        SINR_array(j,2) = delay(j);
+        SINR_array(j,3) = thput(j);
+        SINR_array(j,4) = pos_x(j);
+        SINR_array(j,5) = pos_y(j);
+        
+        RSRP_array(j,1) = RSRP(j); 
+        RSRP_array(j,2) = delay(j);
+        RSRP_array(j,3) = thput(j);     
+        RSRP_array(j,4) = pos_x(j);
+        RSRP_array(j,5) = pos_y(j);
+    end
+    SINR_array = sortrows(SINR_array,1);
+    RSRP_array = sortrows(RSRP_array,1);
+    
+%Irasome rezultatus pagal SINR:
+    %SINR_save_file = fopen('SINR_MAP.txt', 'at');
+    SINR_save_file = fopen(SINR_file, 'at');
+    for j = 1:p(2)
+        if ~(SINR_array(j,2) ==0)
+            fprintf(SINR_save_file, '%f ', SINR_array(j,1)); 
+            fprintf(SINR_save_file, '%f ', SINR_array(j,2)); 
+            fprintf(SINR_save_file, '%f ', SINR_array(j,3));
+            fprintf(SINR_save_file, '%f ', SINR_array(j,4));
+            fprintf(SINR_save_file, '%f \n', SINR_array(j,5));
+        end
+    end               
+    fclose(SINR_save_file);
+    
+%Irasome rezultatus pagal RSRP:
+    %RSRP_save_file = fopen('RSRP_MAP.txt', 'at');
+    RSRP_save_file = fopen(RSRP_file, 'at');    
+    for j = 1:p(2)
+        if ~(RSRP_array(j,2) ==0)
+            fprintf(RSRP_save_file, '%f ', RSRP_array(j,1)); 
+            fprintf(RSRP_save_file, '%f ', RSRP_array(j,2)); 
+            fprintf(RSRP_save_file, '%f ', RSRP_array(j,3));           
+            fprintf(RSRP_save_file, '%f ', RSRP_array(j,4));
+            fprintf(RSRP_save_file, '%f \n', RSRP_array(j,5));
+        end
+    end               
+    fclose(RSRP_save_file);    
+end
